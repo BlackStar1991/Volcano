4399610947578ece4c7e40bc2e0e3ff56a93f90e
DELL-PC\Andry Zirka <zirkaandry@gmail.com>
2018-01-04 10:29:29 +0200

    finished test work

--- dist/js/common.min.js
+++ dist/js/common.min.js
@@ -8,72 +8,28 @@ window.onload = function() {
 
     var companyList = getCompanyList();
 
-
-
     ///////// btn Pointer
-    var counterPoints = 0;
-
-    btnPoint.on("click", function (){
-        counterPoints++;
-
-        if(counterPoints %2 === 0){
-            pointSortDirection = true;
-            byPoint = sortArrayByPoint(companyList, pointSortDirection);
-
-            for (var i = 0; i<byPoint.length; i++) {
-
-                $(".bl_companies__item").eq(i).html(byPoint[i].innerHtml);
-            }
-
-        }else{
-            pointSortDirection = false;
-            byPoint = sortArrayByPoint(companyList, pointSortDirection);
-
-            for (var i =0; i<companiesLength; i++){
-                $(".bl_companies__item").eq(i).html(byPoint[i].innerHtml);
-            }
-        }
+    btnPoint.on("click", function () {
+        pointSortDirection=!pointSortDirection;
+        byPoint = sortArrayByField(companyList, pointSortDirection, 'point');
+        renderCompanyListHtml(byPoint);
     });
 
-
-
-
-
-/////// btn Raiting
-    var counterRating = 0;
-
-    btnRating.on("click", function (){
-        counterRating++;
-
-        if(counterRating %2 === 0){
-
-            pointSortDirection = true;
-            byRating = sortArrayByRating(companyList, ratingSortDirection);
-            for (var i =0; i<companiesLength; i++){
-                $(".bl_companies__item").eq(i).html(byRating[i].innerHtml);
-            }
-
-        }else{
-            pointSortDirection = false;
-            byRating = sortArrayByRating(companyList, ratingSortDirection);
-            for (var i = 0; i<byRating.length; i++) {
-                $(".bl_companies__item").eq(i).html(byRating[i].innerHtml);
-            }
-
-        }
+    /////// btn Raiting
+    btnRating.on("click", function () {
+        ratingSortDirection=!ratingSortDirection;
+        byRating = sortArrayByField(companyList, ratingSortDirection, 'rating');
+        renderCompanyListHtml(byRating);
     });
 
-
-    // DESC
-
-
-
-    // console.log(byPoint.reverse());
-    // console.log(byRating.reverse());
-
+    function renderCompanyListHtml(companyList) {
+        for (var i = 0; i<companyList.length; i++) {
+            $(".bl_companies__item").eq(i).html(companyList[i].innerHtml);
+        }
+    }
 
     function getCompanyList() {
-        var companyList = new Array();
+        var companyList = [];
         for (var i=0; i< companiesLength; i++) {
             var rating = $(".bl_companies__rating_current").eq(i).text();
             var point =$(".bl_companies__recoil").eq(i).text().replace(',', '.');
@@ -85,41 +41,19 @@ window.onload = function() {
         return companyList;
     }
 
-    function sortArrayByRating(arrayToSort, direction) {
-        // use slice() to copy the array and not just make a reference
-        var byRating = arrayToSort.slice(0);
-        byRating.sort(function(a,b) {
-
-            if(direction === true){
-                return  a.rating - b.rating;
-            }else{
-                return  b.rating - a.rating;
+    function sortArrayByField(arrayToSort, direction, field) {
+        // Use slice() to copy the array and not just make a reference
+        var byField = arrayToSort.slice(0);
+        byField.sort(function(a,b) {
+            if (direction === true) {
+                return  a[field] - b[field];
+            } else {
+                return  b[field] - a[field];
             }
         });
 
-        return byRating;
+        return byField;
     }
-
-
-
-
-    function sortArrayByPoint(arrayToSort, direction) {
-        // use slice() to copy the array and not just make a reference
-        var byPoint = arrayToSort.slice(0);
-        byPoint.sort(function(a,b) {
-            if(direction === true){
-                return  a.point - b.point;
-            }else{
-                return  b.point - a.point;
-            }
-        });
-
-        return byPoint;
-    }
-
-
-
-
 };
 
 
